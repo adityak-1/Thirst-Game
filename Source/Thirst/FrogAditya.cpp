ac99923f25551499fc3d8263091bef90ffa5a647
Aditya Kaliappan <adityak1@umbc.edu>
2019-03-03 19:30:50 -0500

    Added Basic Left/Right/Jump Movement

--- Source/Thirst/FrogAditya.cpp
+++ Source/Thirst/FrogAditya.cpp
@@ -1,6 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//Name: Aditya Kaliappan
+//File: FrogAditya.cpp
+//
+//This file contains basic motion functions for the main player.
+//
+//References:
+//http://api.unrealengine.com/INT/API/Runtime/Engine/Components/UInputComponent/BindAction/1/index.html
+//https://wiki.unrealengine.com/First_Person_Shooter_C%2B%2B_Tutorial
+//https://answers.unrealengine.com/questions/212373/character-rotation-problem-c.html
 
 #include "FrogAditya.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
 AFrogAditya::AFrogAditya()
@@ -14,14 +23,36 @@ AFrogAditya::AFrogAditya()
 void AFrogAditya::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	isRight = false;
+	moveScale = 500;
 }
 
 // Called every frame
 void AFrogAditya::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	Jump();
+
+	/*float xVel = GetVelocity().X;
+
+	if (xVel < 0) {
+		isRight = false;
+
+		FRotator flip(0.0f, 180.0f, 0.0f);
+		SetActorRotation(flip);
+	} else if (xVel > 0) {
+		isRight = true;
+
+		FRotator flip(0.0f, 0.0f, 0.0f);
+		SetActorRotation(flip);
+	}*/
+
+	/*float xVel = GetVelocity().X;
+	bool flipSprite = (xVel < 0 && isRight) || (xVel > 0 && !isRight);
+
+	if (flipSprite) {
+		FRotator flip(0.0f, 180.0f, 0.0f);
+		SetActorRotation(flip);
+	}*/
 }
 
 // Called to bind functionality to input
@@ -29,5 +60,17 @@ void AFrogAditya::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	//bind movement functions to input controls
+	PlayerInputComponent->BindAxis("LeftRight", this, &AFrogAditya::MoveLeftRight);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump); 
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 }
 
+void AFrogAditya::MoveLeftRight(float dir)
+{
+	if (dir != 0.0f) {
+		//dir = (isRight) ? dir : -dir;
+
+		AddMovementInput(GetActorForwardVector() * moveScale, dir);
+	}
+}
--- Source/Thirst/FrogAditya.h
+++ Source/Thirst/FrogAditya.h
@@ -15,15 +15,21 @@ public:
 	// Sets default values for this character's properties
 	AFrogAditya();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// function to handle left/right motion of player
+	UFUNCTION()
+		void MoveLeftRight(float val);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	bool isRight;
+	int moveScale;
 };
