4cd2ccb5cdbf00ed83b2f63252f5bbc0ac35a42f
Aditya Kaliappan <adityak1@umbc.edu>
2019-03-09 20:19:06 -0500

    Added Basic Slither Movement to Snake

--- Source/Thirst/Snake.cpp
+++ Source/Thirst/Snake.cpp
@@ -1,4 +1,75 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+//Name: Aditya Kaliappan
+//File: Snake.cpp
+//
+//This file contains the AI functionality for the snake enemy.
 
 #include "Snake.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "Engine/GameEngine.h"
 
+// Sets default values
+ASnake::ASnake()
+{
+	//set this character to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	isRight = true;
+}
+
+// Called when the game starts or when spawned
+void ASnake::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//get the position of snake as movement center
+	center = GetCharacterMovement()->GetActorLocation();
+}
+
+// Called every frame
+void ASnake::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//get X component of snake velocity
+	float xVel = GetVelocity().X;
+
+	//compute rotation for snake to face correct direction
+	float yawAngle = (xVel > 0.0f) ? 180.0f : ((xVel < 0.0f) ? 0.0f :
+		GetControlRotation().Yaw);
+
+	//rotate player based on direction of motion
+	SetActorRotation(FRotator(0.0f, yawAngle, 0.0f));
+	
+	//update player movement
+	Slither();
+
+	//set next animation state
+	UPaperFlipbook* currAnim;
+
+	//set animation to slither
+	currAnim = slitherAnim;
+
+	//update player animation if incorrect
+	if (GetSprite()->GetFlipbook() != currAnim) {
+		GetSprite()->SetFlipbook(currAnim);
+	}
+}
+
+void ASnake::Slither() {
+	//move snake slightly in current frame
+	AddMovementInput(FVector(speedScale, 0.0f, 0.0f), (isRight ? 1.0f : -1.0f));
+
+	//get current X position
+	float currX = GetCharacterMovement()->GetActorLocation().X;
+
+	//get displacement from starting point
+	float disp = currX - center.X;
+
+	//constrain snake movement to specified radius
+	if ((isRight && disp > moveRadius) || (!isRight && -disp > moveRadius)) {
+		GetCharacterMovement()->StopMovementImmediately();
+		isRight = !isRight;
+	}
+}
--- Source/Thirst/Snake.h
+++ Source/Thirst/Snake.h
@@ -13,5 +13,39 @@ UCLASS()
 class THIRST_API ASnake : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this character's properties
+	ASnake();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// functions to handle snake slither
+	UFUNCTION()
+		void Slither();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//scale for snake speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float speedScale;
+
+	//radius that snake can travel within
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float moveRadius;
+
+	//animation when player is slithering
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* slitherAnim;
+
+	//animation when player is lunging
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* lungeAnim;
+
+private:
+	FVector center;
+	bool isRight;
 };
