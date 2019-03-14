13f75b316ef9cf25d3170061bf7af7273e1d8ca0
Jack Wang <wzn1234234@gmail.com>
2019-03-13 21:34:05 -0400

    merge for adding scarab

--- Source/Thirst/Scarab.cpp
+++ Source/Thirst/Scarab.cpp
@@ -1,4 +1,144 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*Author: Jack Wang
+ *File: Scarab.cpp
+ *
+ *This file contains the AI functionality for the scarab enemy.
+ */
 
 #include "Scarab.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "Engine/GameEngine.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
+// Sets default values
+AScarab::AScarab()
+{
+	//set this character to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	isRight = true;
+	isHigh = false;
+}
+
+// Called when the game starts or when spawned
+void AScarab::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//get the position of scarab as movement center
+	center = GetCharacterMovement()->GetActorLocation();
+
+	//set scarab to target player
+	enemy = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+// Called every frame
+void AScarab::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//if hit points go to or below 0, destroy this actor
+	if (hitPoints <= 0) {
+		this->Destroy();
+	}
+
+	//get X component of scarab velocity
+	float xVel = GetVelocity().X;
+
+	//compute rotation for scarab to face correct direction
+	float yawAngle = (xVel > 0.0f) ? 180.0f : ((xVel < 0.0f) ? 0.0f :
+		GetControlRotation().Yaw);
+
+	//rotate player based on direction of motion
+	SetActorRotation(FRotator(0.0f, yawAngle, 0.0f));
+
+	//scarab has detected player
+	if (CanBite()) {
+		Bite();
+	}
+	//scarab does not find player
+	else {
+		Sliding();
+	}
+
+	//set next animation state
+	UPaperFlipbook* currAnim;
+
+	//set animation to slither
+	currAnim = slidingAnim;
+
+	//update player animation if incorrect
+	if (GetSprite()->GetFlipbook() != currAnim) {
+		GetSprite()->SetFlipbook(currAnim);
+	}
+}
+
+void AScarab::Sliding() {
+	//determine moving direction
+	if (isHigh) {
+		if (isRight)
+			AddMovementInput(FVector(speedScale, 0.0f, 0.0f), 1.4f);
+		else
+			AddMovementInput(FVector(speedScale, 0.0f, 0.0f), -1.4f);
+	}
+	else {
+		if (isRight)
+			AddMovementInput(FVector(speedScale, 0.0f, speedScale), 1.4f);
+		else
+			AddMovementInput(FVector(speedScale, 0.0f, -speedScale), -1.4f);
+	}
+
+	//AddMovementInput(FVector(speedScale, 0.0f, speedScale), (isRight ? 1.0f : -1.0f));
+
+	//get current X position
+	float currX = GetCharacterMovement()->GetActorLocation().X;
+
+	//get displacement from starting point
+	float dispX = currX - center.X;
+
+	//get current Z position
+	float currZ = GetCharacterMovement()->GetActorLocation().Z;
+
+	//get displacement from starting point
+	float dispZ = currZ - center.Z;
+
+	//constrain scarab movement to specified radius
+	if ((dispZ > moveHeight*2)|| (dispZ < 10.0f)) {
+		GetCharacterMovement()->StopMovementImmediately();
+		isHigh = !isHigh;
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("isHigh changed!"));
+	}
+
+	if ((isRight && dispX > moveRadius) || (!isRight && -dispX > moveRadius)) {
+		GetCharacterMovement()->StopMovementImmediately();
+		isRight = !isRight;
+	}
+		
+}
+
+float AScarab::GetPlayerDisp() {
+	//get current X position
+	float currX = GetCharacterMovement()->GetActorLocation().X;
+
+	return enemy->GetActorLocation().X - currX;
+}
+
+bool AScarab::CanBite() {
+	//get displacement to player
+	float disp = GetPlayerDisp();
+
+	return abs(disp) <= visionDist && (isRight != (disp < 0));
+}
+
+void AScarab::Bite() {
+	//update scarab to face player
+	SetActorRotation(FRotator(0.0f, (isRight ? 180.0f : 0.0f), 0.0f));
+}
+
+//called when an attack hits the scarab
+void AScarab::Damage(int damageTaken) {
+	hitPoints -= damageTaken;
+}
--- Source/Thirst/Scarab.h
+++ Source/Thirst/Scarab.h
@@ -14,4 +14,64 @@ class THIRST_API AScarab : public APaperCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	// Sets default values for this character's properties
+	AScarab();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// function to get displacement to player
+	UFUNCTION()
+		float GetPlayerDisp();
+
+	// function to handle snake sliding
+	UFUNCTION()
+		void Sliding();
+
+	// functions to handle snake lunge
+	UFUNCTION()
+		bool CanBite();
+	void Bite();
+
+	//function for snake to take damage
+	UFUNCTION()
+		void Damage(int damageTaken);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//scale for snake speed
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float speedScale;
+
+	//radius that snake can travel within
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float moveRadius;
+
+	//radius that snake can travel within
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float moveHeight;
+
+	//distance that snake can see ahead
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+		float visionDist;
+
+	//animation when player is slithering
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* slidingAnim;
+
+	//animation when player is lunging
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* biteAnim;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+		int hitPoints;
+
+private:
+	FVector center;
+	APawn* enemy;
+	bool isRight;
+	bool isHigh;
 };
